bc188fd872c266754a933f1c6211d30295b13564
Eric Erhardt <eric.erhardt@microsoft.com>
2024-11-06 17:41:54 -0600

    Update copyright headers

--- src/Aspire.Hosting.AWS/CloudFormation/StackOutputReference.cs
+++ src/Aspire.Hosting.AWS/CloudFormation/StackOutputReference.cs
@@ -1,5 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 using Aspire.Hosting.ApplicationModel;
 
